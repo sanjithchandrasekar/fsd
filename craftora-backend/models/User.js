028de2dbf85b43e53df030df3fa1956d0d97acca
sanjithchandrasekar <sanjithchandrasekar03.com>
2026-04-28 22:29:17 +0530

    feat: use plain text passwords and disable bcrypt hashing

--- craftora-backend/models/User.js
+++ craftora-backend/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -63,15 +62,9 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Hash password before saving
-userSchema.pre('save', async function () {
-  if (!this.isModified('password')) return;
-  this.password = await bcrypt.hash(this.password, 12);
-});
-
-// Compare password method
+// Plain text password comparison (No security as requested)
 userSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+  return enteredPassword === this.password;
 };
 
 module.exports = mongoose.model('User', userSchema);
